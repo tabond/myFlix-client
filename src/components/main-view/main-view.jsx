b54c00fe3e0985d2d873bb63221f9bb830612c4d
tabond <30688315+tabond@users.noreply.github.com>
2023-12-11 17:37:47 +1000

    pull data from api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,42 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      description:
-        "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.",
-      image:
-        "https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=tt_ov_i",
-      genre: "Action",
-      director: "Christopher Nolan",
-    },
-
-    {
-      id: 2,
-      title: "The Shawshank Redemption",
-      description:
-        "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
-      image:
-        "https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i",
-      genre: "Drama",
-      director: "Frank Darabont",
-    },
-    {
-      id: 3,
-      title: "Gladiator",
-      description:
-        "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
-      image: "https://via.placeholder.com/150",
-      genre: "Action",
-      director: "Ridley Scott",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://my-flix-24-1621e73cc837.herokuapp.com")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            _id: doc._id,
+            Title: doc.Title,
+            Description: doc.Description,
+            Genre: doc.Genre,
+            Director: doc.Director,
+            ImagePath: doc.ImagePath,
+            Featured: doc.Featured,
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
