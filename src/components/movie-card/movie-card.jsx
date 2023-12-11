9284f2ea7996a14bff56de24b46977367b85c789
tabond <30688315+tabond@users.noreply.github.com>
2023-12-11 11:14:51 +1000

    prettier fixed

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,10 @@
-export const MovieCard = ({movie, onMovieClick}) => {
-    return (
-        <div onClick={() => {
-            onMovieClick(movie);
-        }}>
-            {movie.title}</div>
-    );
+export const MovieCard = ({ movie, onMovieClick }) => {
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}>
+      {movie.title}
+    </div>
+  );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-import { MainView } from './components/main-view/main-view';
+import { MainView } from "./components/main-view/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-  return (
-    <MainView />
-  );
+  return <MainView />;
 };
 
 // Finds the root of your app
